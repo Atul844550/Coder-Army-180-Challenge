07fe37b4b9081e5c591884c159711ebdaed942e5
Atul Soni <118369556+Atul844550@users.noreply.github.com>
2023-09-26 15:50:22 +0530

    Create Q_2.cpp

---
+++ Day_3(C++ introduction)/Q_2.cpp
@@ -0,0 +1,13 @@
+#include<iostream>
+using namespace std;
+
+int main()
+{
+    int a, b;
+    cout<<"enter the first no.";
+    cin>>a;
+    cout<<"enter the second no.";
+    cin>>b;
+    cout<<"subtraction of a and b is"<<a-b;
+}
+
