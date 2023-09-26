524a8fe25f91e2fbb5cd9025bf5d88524df73533
Atul Soni <118369556+Atul844550@users.noreply.github.com>
2023-09-26 16:38:53 +0530

    Create if_else_Q1.cpp

---
+++ Day_4(if else and for loop)/if_else_Q1.cpp
@@ -0,0 +1,15 @@
+#include<iostream>
+using namespace std;
+
+int main()
+{
+    int a=30, b=45;
+    if(a<b)
+    {
+        cout<<"b is greater:"<<b<<endl;
+
+    }
+    else{
+        cout<<"a is greater:"<<a<<endl;
+    }
+}
