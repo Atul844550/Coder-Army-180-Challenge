eba799293b5491a9b25336914a30f75f74146936
Atul Soni <118369556+Atul844550@users.noreply.github.com>
2023-09-26 16:40:00 +0530

    Create if_else_Q2.cpp

---
+++ Day_4(if else and for loop)/if_else_Q2.cpp
@@ -0,0 +1,19 @@
+#include<iostream>
+using namespace std;
+
+int main()
+{
+    int age;
+    cout<<"enter your age:"<<endl;
+    cin>>age;
+
+    if(age>=18)
+    {
+        cout<<"Adult";
+    }
+    else{
+        cout<<"Tenager";
+    }
+
+
+}
