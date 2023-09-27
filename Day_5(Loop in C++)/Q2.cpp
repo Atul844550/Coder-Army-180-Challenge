2d8c59d3722e102e8dea0ca0638ea714fc4f2e95
Atul Soni <118369556+Atul844550@users.noreply.github.com>
2023-09-27 19:26:55 +0530

    Create Q2.cpp

---
+++ Day_5(Loop in C++)/Q2.cpp
@@ -0,0 +1,12 @@
+#include<iostream>
+using namespace std;
+
+int main()
+{
+    cout<<"the character a to z printed below are:"<<endl;
+
+    for(char i='a'; i<='z'; i++)
+    {
+        cout<<i<<endl;
+    }
+}
