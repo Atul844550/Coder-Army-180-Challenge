c7c756380aa521efb7c823f71bf63c9f0818f751
Atul Soni <118369556+Atul844550@users.noreply.github.com>
2023-09-27 19:27:57 +0530

    Create Q4.cpp

---
+++ Day_5(Loop in C++)/Q4.cpp
@@ -0,0 +1,13 @@
+#include<iostream>
+using namespace std;
+
+int main()
+{
+    cout<<"the AP series mention below is"<<endl;
+
+    for(int i=220; i<=730; i+=7)
+    {
+        cout<<i<<endl;
+    
+    }
+}
