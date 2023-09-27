e7725a47492631d2a239a6900e301ca9ea523615
Atul Soni <118369556+Atul844550@users.noreply.github.com>
2023-09-27 19:26:06 +0530

    Create Q1.cpp

---
+++ Day_5(Loop in C++)/Q1.cpp
@@ -0,0 +1,13 @@
+#include<iostream>
+using namespace std;
+
+int main()
+{
+    cout<<"the numbers between 280 to 250 mentioned below are:"<<endl;
+    
+    for(int i=280; i>=250; i-- )
+    {
+        cout<<i<<endl;
+        
+    }
+}
