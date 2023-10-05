449f38aaf036db585c9eb6d05507b022a781e719
Atul Soni <118369556+Atul844550@users.noreply.github.com>
2023-10-05 19:17:10 +0530

    Create Q2.cpp

---
+++ 8. Days_12(operators in C++)/Q2.cpp
@@ -0,0 +1,20 @@
+#include<iostream>
+using namespace std;
+
+int main()
+{
+    int n;
+    cout<<"enter the number:"<<endl;
+    cin>>n;
+
+    if(n>0 && n%2==0)
+    {
+        cout<<"yes"<<endl;
+    }
+    else
+    {
+        cout<<"no";
+    }
+
+
+}
