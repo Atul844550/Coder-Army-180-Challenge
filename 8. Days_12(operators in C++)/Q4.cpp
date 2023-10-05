cf1c272ea780aea49f5b8b4de324a774e18bb1eb
Atul Soni <118369556+Atul844550@users.noreply.github.com>
2023-10-05 19:17:58 +0530

    Create Q4.cpp

---
+++ 8. Days_12(operators in C++)/Q4.cpp
@@ -0,0 +1,18 @@
+#include<iostream>
+using namespace std;
+
+int main()
+{
+    int a, b, c;
+    cout<<"enter the value of a b and c"<<endl;
+    cin>>a>>b>>c;
+
+    if(a>b || a>c)
+    {
+        cout<<"yes"<<endl;
+    }
+    else{
+        cout<<"no";
+    }
+
+}
