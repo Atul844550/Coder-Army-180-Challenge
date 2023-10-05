f7be50c4074722bf63abed4d6a8f85981f988e59
Atul Soni <118369556+Atul844550@users.noreply.github.com>
2023-10-05 19:17:31 +0530

    Create Q3.cpp

---
+++ 8. Days_12(operators in C++)/Q3.cpp
@@ -0,0 +1,18 @@
+#include<iostream>
+using namespace std;
+
+int main()
+{
+    int age;
+    cout<<"enter the age:"<<endl;
+    cin>>age;
+
+    if(age >=13 && age<= 19)
+    {
+        cout<<"a person is teenager"<<endl;
+    }
+    else
+    {
+        cout<<"a person is not teenager";
+    }
+}
