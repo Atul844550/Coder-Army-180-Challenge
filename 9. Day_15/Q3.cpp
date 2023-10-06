801b885c9265b22298e4959b40390ad16a9ea0a6
Atul Soni <118369556+Atul844550@users.noreply.github.com>
2023-10-06 14:36:15 +0530

    Create Q3.cpp

---
+++ 9. Day_15/Q3.cpp
@@ -0,0 +1,28 @@
+#include<iostream>
+using namespace std;
+
+int main()
+{
+    int n, i=1;
+    cout<<"enter the number"<<endl;
+    cin>>n;
+
+    while(i<=n)
+    {
+        if(i%3!=0 && i%5!=0)
+        {
+              cout<<i<<" ";
+            
+        }
+        i++;
+    }
+
+       
+
+
+
+   
+    
+
+    
+}
