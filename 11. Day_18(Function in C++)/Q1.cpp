73f1dd92b896981d1579f437fe1afa042c4c102c
Atul Soni <118369556+Atul844550@users.noreply.github.com>
2023-10-08 19:39:06 +0530

    Create Q1.cpp

---
+++ 11. Day_18(Function in C++)/Q1.cpp
@@ -0,0 +1,18 @@
+#include<iostream>
+using namespace std;
+
+int cube(int n)
+{ 
+    return n*n*n;
+}
+
+int main()
+{
+    
+    int a;
+    cout<<"enter the number:";
+    cin>>a;
+    cout<<cube(a);
+
+
+}
