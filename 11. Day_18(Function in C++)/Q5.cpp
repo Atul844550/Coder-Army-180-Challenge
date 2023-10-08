539e2e51deef589a705c0eb8cec95080c2b884d7
Atul Soni <118369556+Atul844550@users.noreply.github.com>
2023-10-08 19:40:49 +0530

    Create Q5.cpp

---
+++ 11. Day_18(Function in C++)/Q5.cpp
@@ -0,0 +1,22 @@
+#include<iostream>
+using namespace std;
+
+void printing()
+{
+    int n;
+    cout<<"enter the number:"<<endl;
+    cin>>n;
+    for(int i=1; i<=n; i++)
+    {
+        cout<<"Hello coder army"<<endl;
+    }
+}
+
+
+
+
+int main()
+{
+    printing();
+
+}
